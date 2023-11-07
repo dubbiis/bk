153778205ccafc077d37fe8775880c57da115322
dubbis <dagarmar92@gmail.com>
2023-11-07 21:55:03 +0100

    subida

--- app/src/main/java/com/pmym/myapplication/MainActivity.kt
+++ app/src/main/java/com/pmym/myapplication/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.pmym.myapplication
 
+import android.app.Fragment
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +21,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Icon
@@ -31,6 +40,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
 import com.pmym.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,31 +60,124 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
-private fun SootheNavigationRail(modifier: Modifier = Modifier) {
+fun carruseldos(
+    modifier: Modifier = Modifier
+) {
+    // Implement composable here
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.height(168.dp)
+    )
+    {
+        items(carrusel_dos) { item ->
+            carrusel(item.drawable)
 
-    Row {
-        Column {
-            Image(
-                painter = painterResource(id = R.drawable.home),
-                contentDescription =null)
-            Text(text = "home")
-        }
-        Spacer(modifier=Modifier.width(20.dp)) //este spacer me separa los iconos
-        Column {
-            Image(
-                painter = painterResource(id = R.drawable.mybk),
-                contentDescription =null,
-                modifier = Modifier
-                    .fillMaxSize()))
-                    .wrapContentSize(Alignment.Center ))
-                Text(text = "My Burger King")
         }
     }
+    @Composable
+    fun burguercercanos(modifier: Modifier = Modifier) {
+        Row {
+            Column {
+                Text(text = "TU BK MAS CERCANO")
+            }
+            Spacer(modifier = Modifier.width(180.dp))
+            Column {
+                Text(text = "VER TODOS")
+            }
+        }
 
+
+    }
+
+
+    @Composable
+    fun SootheNavigationRail(modifier: Modifier = Modifier) {
+
+        Row {
+            Column {
+                Image(
+                    painter = painterResource(id = R.drawable.home),
+                    contentDescription = null
+                )
+                Text(text = "Home")
+            }
+            Spacer(modifier = Modifier.width(20.dp)) //este spacer me separa los iconos
+            Column {
+                Image(
+                    painter = painterResource(id = R.drawable.mybk),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(90.dp)
+                )
+                Text(text = "My Burger King")
+            }
+            Spacer(modifier = Modifier.width(20.dp)) //este spacer me separa los iconos
+            Column {
+                Image(
+                    painter = painterResource(id = R.drawable.carta),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(90.dp)
+                )
+                Text(text = "Carta")
+            }
+            Spacer(modifier = Modifier.width(20.dp)) //este spacer me separa los iconos
+            Column {
+                Image(
+                    painter = painterResource(id = R.drawable.servicios),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(90.dp)
+                )
+                Text(text = "Servicios")
+            }
+            Spacer(modifier = Modifier.width(20.dp)) //este spacer me separa los iconos
+            Column {
+                Image(
+                    painter = painterResource(id = R.drawable.perfil),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(90.dp)
+                )
+                Text(text = "Perfil")
+            }
+        }
+
+    }
+
+    private val carrusel_dos = listOf(
+        R.drawable.carruno,
+        R.drawable.carrdos,
+        R.drawable.carrtres,
+        R.drawable.carrcuatro
+
+    ).map { carrusel() }
+
+    private data class carrusel(
+        @DrawableRes val drawable: Int)
+
+
+
+
+@Preview(showBackground = true,  backgroundColor = 0xFFF3CA8D
+)
+@Composable
+fun carruseldosPreview() {
+    carruseldos()
 }
-@Preview(showBackground = true,  backgroundColor = 0xFFECB058
+
+@Preview(showBackground = true,  backgroundColor = 0xFFF3CA8D
+)
+@Composable
+fun burguercercanosPreview() {
+    burguercercanos()
+}
+
+@Preview(showBackground = true,  backgroundColor = 0xFFF3CA8D
 )
 @Composable
 fun SootheNavigationRailPreview() {
--- app/src/main/java/com/pmym/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/pmym/myapplication/ui/theme/Color.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 val amarilloburguer = Color(0xFFFFC107)
 val rojoburger = Color(0xFFF44336)
 val marronburger = Color(0xFF7E2D14)
-val fondo = Color (0xFFECB058)
+val fondo = Color(0xFFF3CA8D)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
